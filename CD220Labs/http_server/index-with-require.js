24d8548f1f18ababed386c1111016466723b1ce7
DigvijayMandal <digvijaymandal19051999@gmail.com>
2021-06-04 03:36:19 +0000

    Changes made

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,7 +3,22 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  var dateval= today.getDate();
+  var greet= "It is still dark"
+  if(dateval.getHours()>6 && dateval.getHours()<12)
+  {
+      greet = "Good Morning Digvijay!"
+  }else if(dateval.getHours()==12){
+      greet = "Good Noon Digvijay!"
+  }else if(dateval.getHours()>12 && dateval.getHours()<18){
+      greet = "Good Afternoon Digvijay!"
+  }else if(dateval.getHours()>=18 && dateval.getHours()<21){
+      greet = "Good Evening Digvijay!"
+  }else if(dateval.getHours()>=21 && dateval.getHours()<24)
+  {
+      greet = "Good NigHt Digvijay!"
+  }
+  res.end(`Hello, ${greet}`)
 }
 
 const port = 8080;
